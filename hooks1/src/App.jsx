d94eee9bd940f3f89298415624accc6a4f54bdbc
aditi <additigupta366@gmail.com>
2023-12-28 22:01:27 +0530

    hooks basic digital clock

--- hooks1/src/App.jsx
+++ hooks1/src/App.jsx
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 
-const App=()=>{
-let time=new Date().toLocaleTimeString();
-const [ctime,setctime]=useState(time);
-const changetime=()=>{
-    let newtime=new Date().toLocaleTimeString();
-    setctime(newtime);
 
-};
-return(
-<>
-<div className="container">
-<h1>{ctime}</h1>
-    <button onClick={changetime}>view time</button>
-</div>    
-</>
-);
+// ============DIGITAL CLOCK=========
+const App=()=>{
+    let [count,setcount]=useState(0);
+
+    const incNum=()=>{
+        setcount(count+1);
+    }
+    let time=new Date().toLocaleTimeString();
+    let [ctime,setctime]=useState(time);
+    const update=()=>{
+        time=new Date().toLocaleTimeString();
+        setctime(time);
+    }
+    setInterval(update,1000);
+
+    return (
+        <>
+            <div className="container">
+
+            <h2>{count}</h2>
+                <button onClick={incNum}>update</button>
+                <p>tap this button to increase the no by 1</p>
+                <h1>{ctime}</h1>
+                
+                
+                
+            </div>
+        </>
+    );
+
 };
 export default App;
